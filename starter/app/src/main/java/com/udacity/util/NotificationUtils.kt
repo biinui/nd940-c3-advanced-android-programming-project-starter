d0e06a721f49409695213df860f2edcdbf3e3f73
biinui <yu.alvinray@gmail.com>
2021-01-18 07:54:00 +0800

    Add initial notification utilities.

---
+++ starter/app/src/main/java/com/udacity/util/NotificationUtils.kt
@@ -0,0 +1,58 @@
+package com.udacity.util
+
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import androidx.core.app.NotificationCompat
+import com.udacity.detail.DetailActivity
+import com.udacity.R
+import com.udacity.receiver.DismissReceiver
+
+private val NOTIFICATION_ID = 0
+private val REQUEST_CODE = 0
+private val FLAGS = 0
+
+fun NotificationManager.sendNotification(messageBody: String, applicationContext: Context) {
+
+    val contentIntent = Intent(applicationContext, DetailActivity::class.java)
+    val contentPendingIntent = PendingIntent.getActivity(
+        applicationContext,
+        NOTIFICATION_ID,
+        contentIntent,
+        PendingIntent.FLAG_UPDATE_CURRENT
+    )
+
+    val dismissIntent = Intent(applicationContext, DismissReceiver::class.java)
+    val dismissPendingIntent: PendingIntent = PendingIntent.getBroadcast(
+        applicationContext,
+        REQUEST_CODE,
+        dismissIntent,
+        FLAGS)
+
+    val builder = NotificationCompat.Builder(
+        applicationContext,
+        applicationContext.getString(R.string.download_notification_channel_id)
+    )
+
+        .setSmallIcon(R.drawable.ic_launcher_foreground)
+        .setContentTitle(applicationContext.getString(R.string.notification_title))
+        .setContentText(messageBody)
+
+        .setContentIntent(contentPendingIntent)
+        .setAutoCancel(true)
+
+        .addAction(
+            R.drawable.ic_launcher_foreground,
+            applicationContext.getString(R.string.dismiss),
+            dismissPendingIntent
+        )
+
+        .setPriority(NotificationCompat.PRIORITY_HIGH)
+
+    notify(NOTIFICATION_ID, builder.build())
+}
+
+fun NotificationManager.cancelNotifications() {
+    cancelAll()
+}
